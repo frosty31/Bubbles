e75a349127ce04822102fe87b34f386712894582
Lem <Lem@dhcp-wifi-8021x-155-41-55-129.bu.edu>
2018-04-14 10:40:57 -0400

    added first scene

---
+++ Assets/Scripts/CapsuleController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapsuleController : MonoBehaviour {
+
+    public float speed = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.position += Vector3.left*speed*Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.position += Vector3.right*speed*Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.position += Vector3.up*speed*Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.position += Vector3.down*speed*Time.deltaTime;
+		}
+	}
+}
