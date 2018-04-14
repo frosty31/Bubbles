4d86fe3b3e60cbb145dabe66ad4fce60e62f2a36
frosty31 <liambeck@bu.edu>
2018-04-14 12:36:57 -0400

    single player basic functioning

--- Assets/Scripts/CapsuleController.cs
+++ Assets/Scripts/CapsuleController.cs
@@ -2,32 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CapsuleController : MonoBehaviour {
+public class CapsuleController : MonoBehaviour
+{
 
-    public float speed = 1.5f;
+    public float speed;
 
-	// Use this for initialization
-	void Start () {
-        Debug.Log("Hello");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKey(KeyCode.LeftArrow))
-		{
-			transform.position += Vector3.left*speed*Time.deltaTime;
-		}
-		if (Input.GetKey(KeyCode.RightArrow))
-		{
-			transform.position += Vector3.right*speed*Time.deltaTime;
-		}
-		if (Input.GetKey(KeyCode.UpArrow))
-		{
-			transform.position += Vector3.up*speed*Time.deltaTime;
-		}
-		if (Input.GetKey(KeyCode.DownArrow))
-		{
-			transform.position += Vector3.down*speed*Time.deltaTime;
-		}
-	}
+    public GameObject bubble;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 bubbleDirection = Camera.main.transform.forward;
+            Vector3 bubblePosition = Camera.main.transform.position + bubbleDirection * 1.5f;
+            GameObject nextBubble = (GameObject)Instantiate(bubble, bubblePosition, Quaternion.Euler(bubbleDirection));
+            nextBubble.GetComponentInChildren<Rigidbody>().velocity = bubbleDirection*1.0f;
+
+            Destroy(nextBubble, 8.0f);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.name=="Bubble(Clone)") {
+            Destroy(this.gameObject);
+        }
+            
+            
+    }
 }
